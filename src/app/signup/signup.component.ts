b2cf40ac6a9b5fcb84b835692819039d970e8c6f
SnowTurtle96 <jg.clarke@hotmail.co.uk>
2018-05-20 14:32:29 +0100

    Box shadows added

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -13,6 +13,7 @@ export class SignupComponent implements OnInit {
   public email;
   public passwordSignup;
   public displayName;
+  public errorMessage;
 
   constructor(private db: AngularFireDatabase, private prescence: PrescenceService) {
   }
@@ -24,11 +25,15 @@ export class SignupComponent implements OnInit {
   signUp(email, passwordSignup, displayName) {
     email = email.toString();
     passwordSignup = passwordSignup.toString();
-    firebase.auth().createUserWithEmailAndPassword(email, passwordSignup).then(function (user) {
+    firebase.auth().createUserWithEmailAndPassword(email, passwordSignup).catch( (err: firebase.FirebaseError) => {
+      console.log("Issue with sign in!" + err);
+      this.errorMessage = err;
+    }).then(function (user) {
       return user.updateProfile({
         displayName: displayName
-      }).catch(function (error) {
-        console.log(error);
+      }).catch( (err: firebase.FirebaseError) => {
+        console.log("Issue with sign in!" + err);
+        this.errorMessage = err;
       });
     });
     this.db.database.ref('Accounts').child(displayName);
